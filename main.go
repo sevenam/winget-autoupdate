ba30f995021997e23a1ed7eb5bf17c7f21f7319a
sevenam <johnbt@outlook.com>
2025-06-10 23:18:34 +0200

    move notification code to own service

--- main.go
+++ main.go
@@ -6,7 +6,6 @@ import (
 	"log"
 	"os"
 
-	"github.com/gen2brain/beeep"
 	"golang.org/x/sys/windows/svc"
 )
 
@@ -47,47 +46,5 @@ func main() {
 		runAsService()
 	default:
 		fmt.Println("Unknown command:", os.Args[1])
-	}
-}
-
-func sendNotification(title, message, notificationType string) {
-	var iconPath string
-	switch notificationType {
-	case "error":
-		iconPath = "assets/error.png" // Path to embedded error icon
-	case "success":
-		iconPath = "assets/success.png" // Path to embedded success icon
-	case "info":
-		iconPath = "assets/info.png" // Path to embedded info icon
-	default:
-		iconPath = ""
-	}
-
-	iconData, err := assets.ReadFile(iconPath)
-	if err != nil {
-		logMessage(fmt.Sprintf("Failed to read icon file: %v", err))
-		iconPath = ""
-	} else {
-		// Write the icon data to a temporary file
-		tmpFile, err := os.CreateTemp("", "icon-*.png")
-		if err != nil {
-			logMessage(fmt.Sprintf("Failed to create temp file for icon: %v", err))
-			iconPath = ""
-		} else {
-			defer os.Remove(tmpFile.Name())
-			_, err = tmpFile.Write(iconData)
-			if err != nil {
-				logMessage(fmt.Sprintf("Failed to write icon data to temp file: %v", err))
-				iconPath = ""
-			} else {
-				iconPath = tmpFile.Name()
-			}
-		}
-	}
-
-	// Use the beeep package to send a notification to the Windows notification bar
-	err = beeep.Notify(title, message, iconPath)
-	if err != nil {
-		logMessage(fmt.Sprintf("Failed to send notification: %v", err))
 	}
 }
