9dbe775670173a428baa2fe3ce4a515b24bec103
sevenam <johnbt@outlook.com>
2025-06-10 23:16:56 +0200

    refactoring

--- log_service.go
+++ log_service.go
@@ -9,17 +9,15 @@ import (
 func logMessage(message string) {
 	fmt.Println(message)
 
-	logDir := LogDirectory
-	const logFile = LogFilePath
 	// Ensure the log directory exists
-	if _, err := os.Stat(logDir); os.IsNotExist(err) {
-		err := os.MkdirAll(logDir, os.ModePerm)
+	if _, err := os.Stat(LogDirectory); os.IsNotExist(err) {
+		err := os.MkdirAll(LogDirectory, os.ModePerm)
 		if err != nil {
 			log.Fatal("Error creating log directory:", err)
 		}
 	}
 
-	f, err := os.OpenFile(logFile, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	f, err := os.OpenFile(LogFilePath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
 		log.Fatal("Error opening log file:", err)
 	}
