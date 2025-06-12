6bdbf2d55e350272b837657e1750ec5a7f3631ca
sevenam <johnbt@outlook.com>
2025-06-13 01:15:46 +0200

    bugfix log file

--- constants.go
+++ constants.go
@@ -2,8 +2,8 @@ package main
 
 const (
 	AutoUpdateIntervalEnvVar = "WINGET_AUTOUPDATE_INTERVAL_SECONDS"
-	LogDirectory             = "C:\\ProgramData\\winget-service"
-	LogFilePath              = LogDirectory + "winget-service.log"
+	LogDirectory             = "C:\\ProgramData\\wingetau\\"
+	LogFilePath              = LogDirectory + "wingetau.log"
 	ServiceName              = "Winget-AutoUpdate"
 	WingetExeBasePath        = "C:\\Program Files\\WindowsApps"
 )
